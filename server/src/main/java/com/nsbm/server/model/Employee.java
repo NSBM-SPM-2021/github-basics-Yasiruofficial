a26f7c91511eff06384853c744520b72467ae2a2
Yasiru Swaris <yasiruswaris@gmail.com>
2021-09-01 16:29:15 +0530

    setup DB related modles and their relationships

---
+++ server/src/main/java/com/nsbm/server/model/Employee.java
@@ -0,0 +1,108 @@
+package com.nsbm.server.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique=true)
+    private String eno;
+    private String password;
+    private String nic;
+    private String name;
+    private String address;
+    private String email;
+
+    @OneToMany(mappedBy = "employee")
+    private Set<Attendance> attendances;
+
+    @ManyToOne(cascade = {CascadeType.ALL})
+    private UserRole userRole;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEno() {
+        return eno;
+    }
+
+    public void setEno(String eno) {
+        this.eno = eno;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonManagedReference
+    public Set<Attendance> getAttendances() {
+        return attendances;
+    }
+
+    public void setAttendances(Set<Attendance> attendances) {
+        this.attendances = attendances;
+    }
+
+    @JsonManagedReference
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+}
---
+++ server/src/main/java/com/nsbm/server/model/GrantedAuthority.java
@@ -0,0 +1,52 @@
+package com.nsbm.server.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = {CascadeType.ALL})
+    private UserRole userRole;
+
+    @ManyToOne(cascade = {CascadeType.ALL})
+    private UserPermission userPermission;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @JsonBackReference
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    @JsonManagedReference
+    public UserPermission getUserPermission() {
+        return userPermission;
+    }
+
+    public void setUserPermission(UserPermission userPermission) {
+        this.userPermission = userPermission;
+    }
+
+}

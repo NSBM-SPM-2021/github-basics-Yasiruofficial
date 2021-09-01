abfbe55f6a04c4bc05e6d9f70a0e10f3d4c00555
Yasiru Swaris <yasiruswaris@gmail.com>
2021-09-01 14:53:05 +0530

    Init client and server

---
+++ client/src/app/global-variables.ts
@@ -0,0 +1,11 @@
+import {HttpHeaders} from "@angular/common/http";
+
+
+// export const HOST:string = "http://184.72.214.178:9001/api/";
+export const HOST:string = "http://104.248.97.65:9001/api/";
+
+
+export const httpOptions_Global = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' })
+};
+
